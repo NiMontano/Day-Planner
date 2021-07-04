18a60c4a43aa90fd2faf55abf33bc7aa2ef7bc53
Denise Montano <benita5028@gmail.com>
2021-07-04 18:18:21 -0400

    save txt area to storage, flip lock icon

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-myStorage = window.document
+myStorage = window.localStorage
 
 var today = moment();
 console.log(today);
@@ -10,7 +10,8 @@ console.log (currentHr);
 var plannerHr = document.querySelectorAll("div.row");
 console.log(plannerHr.length);
 
-
+var saveButton= document.querySelectorAll("button");
+console.log(saveButton);
 
 function findTime() {
 for (let i = 0; i < plannerHr.length; i++) {
@@ -19,23 +20,37 @@ for (let i = 0; i < plannerHr.length; i++) {
 
         if (stringHr < currentHr) {
           plannerHr[i].classList.add("past");
-            console.log(plannerHr[i]);
         }
         else if (stringHr > currentHr) {
             plannerHr[i].classList.add("future");
-            console.log(plannerHr[i]);
-
        }
        else if (stringHr = currentHr) {
         plannerHr[i].classList.add("present");
-        console.log(plannerHr[i]);
-
        }
 
     };
 };
 
+function clickSave() {
+  for (let i = 0; i < saveButton.length; i++ ) {
+    saveButton[i].addEventListener("click", function(){
+      var newEvent = this.previousElementSibling.value;
+      var clickedId = this.parentElement.id;
+
+      myStorage.setItem(clickedId, newEvent);
+      for (let i = 0; i < this.children.length; i++){
+        this.children[i].className = "bi bi-lock";
+      }
+
+      // myStorage.setItem()
+      console.log(this.children[0]);
+    });
+  };
+};
+
+
 findTime();
+clickSave();
 
 
 
